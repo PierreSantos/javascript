6e6895727d8d25ffca378a697bcecdce8ff5c847
PierreSantos <121955365+PierreSantos@users.noreply.github.com>
2023-02-16 11:06:16 -0300

    finalizando o exercício de condicional

--- Aula 12ex/ex015/main.js
+++ Aula 12ex/ex015/main.js
@@ -1,21 +1,51 @@
-/*
-    faixas etárias
-    
-    bebê/criança = até 15
-    jovem = de 16 até 25
-    adulto = de 26 até 64
-    idoso = acima de 65
-*/
-
 function verificaridade() {
 
-    //calcula a idade do individuo
+    var res = document.querySelector('div.saida')
+    var foto = document.querySelector('div.foto')
+
+    // Calcula a idade do individuo
     var anoAtual = new Date().getFullYear()
     var anoNasc = document.querySelector('input#ano').value
     var idade = anoAtual - anoNasc
 
     // Verifica qual o sexo do individuo
-    var sexo = document.getElementsByName('sexo').value = 'm' ? 'Homem':'Mulher'
-    document.querySelector('div.saida').innerHTML = `Detectamos ${sexo} com ${idade} anos.`
-}
+    var sexo = document.getElementsByName('sexo')
+    var sexo = sexo[0].checked ? 'Homem' : 'Mulher'
 
+    // Escreve o resultado na tela
+    if (anoNasc <= 0 || anoNasc > anoAtual) {
+        alert('ERRO: Verifique os dados e tente novamente!')
+    } else {
+        res.innerHTML = `Detectamos ${sexo} com ${idade} anos.`
+        if (sexo == 'Homem') {
+            if (idade < 15) {
+                // criança
+                foto.style.backgroundImage = 'url(imagens/bebe-m.jpg)'      
+            } else if (idade < 25) {
+                // jovem
+                foto.style.backgroundImage = 'url(imagens/jovem-m.jpg)'
+            } else if (idade < 64) {
+                // adulto
+                foto.style.backgroundImage = 'url(imagens/adulto-m.jpg)'
+            } else if (idade >= 65) {
+                // idoso
+                foto.style.backgroundImage = 'url(imagens/idoso-m.jpg)'
+            }
+        } else {
+            if (idade < 15) {
+                // criança
+                foto.style.backgroundImage = 'url(imagens/bebe-f.jpg)'
+            } else if (idade < 25) {
+                // jovem
+                foto.style.backgroundImage = 'url(imagens/jovem-f.jpg)'
+            } else if (idade < 64) {
+                // adulto
+                foto.style.backgroundImage = 'url(imagens/adulto-f.jpg)'
+            } else if (idade >= 65) {
+                // idosa
+                foto.style.backgroundImage = 'url(imagens/idoso-f.jpg)'
+            }
+        }
+        foto.style.display = 'block'
+    }
+}
